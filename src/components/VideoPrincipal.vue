f327981be0ba2740fe4a82743b3611573813d1cc
luansilvadb <luan.videira@gmail.com>
2023-10-11 15:26:32 -0300

    yrdyr

--- src/components/VideoPrincipal.vue
+++ src/components/VideoPrincipal.vue
@@ -2,29 +2,43 @@
   <div class="videoprincipal">
     <q-video
       :ratio="16/9"
-      :style="{ border: 'none', borderRadius: '10px' }" src="https://player.twitch.tv/?channel=baiano&parent=multistream-ten.vercel.app&muted=true"
+      :style="{ border: 'none', borderRadius: '10px' }"
+      :src="videoSrc"
     />
+    <button @click="atualizarIframe">Atualizar Twitch</button>
   </div>
 </template>
 
 <script>
 export default {
-  // Adicione opções específicas se necessário
+  data () {
+    return {
+      videoSrc: 'https://player.twitch.tv/?channel=baiano&parent=multistream-ten.vercel.app&muted=true'
+    }
+  },
+  methods: {
+    atualizarIframe () {
+      // Atualizar a URL do iframe
+      // Isso adiciona um parâmetro de data atual à URL para forçar a atualização
+      const novaSrc = `${this.videoSrc}&${Date.now()}`
+      this.videoSrc = novaSrc
+    }
+  }
 }
 </script>
 
 <style scoped>
 .videoprincipal {
   grid-area: 2 / 1 / 4 / 2;
-  width: 100%; /* Largura total em telas menores */
-  height: auto; /* Altura automática para ajustar o conteúdo */
+  width: 100%;
+  height: auto;
   max-width: auto;
+  padding-left: 10px;
 }
 
-/* Estilo para telas menores */
 @media screen and (max-width: 768px) {
   .videoprincipal {
-    grid-area: auto; /* Reverta para fluxo automático na grade */
+    grid-area: auto;
   }
 }
 </style>
